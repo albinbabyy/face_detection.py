cd487be9e3b5d3061411044b2a93ae26db6c3096
Albin Baby <albinbaby150@gmail.com>
2022-05-25 16:04:12 +0200

    complted

--- Python_challenge/Day_2/tip_calculator.py
+++ Python_challenge/Day_2/tip_calculator.py
@@ -6,3 +6,8 @@ tip_percentage = int(input("What percentage tip would you like to give? 10,12 or
 
 people = int(input("How many people to split the bill? : "))
 
+bill_with_tip = total_bill * ((100 + tip_percentage) / 100)
+
+bill_per_person = round(bill_with_tip / people , 2)
+
+print(f"Each person should pay : {bill_per_person}")
