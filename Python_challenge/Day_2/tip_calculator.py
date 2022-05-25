06d95771fc9bd21b942e2f71cc83f09de1446f21
Albin Baby <albinbaby150@gmail.com>
2022-05-25 15:53:26 +0200

    Varibles are defined

---
+++ Python_challenge/Day_2/tip_calculator.py
@@ -0,0 +1,8 @@
+print("Welcome to Tip Calculator")
+
+total_bill = float(input("What was the total bill? : "))
+
+tip_percentage = int(input("What percentage tip would you like to give? 10,12 or 15? : "))
+
+people = int(input("How many people to split the bill? : "))
+
